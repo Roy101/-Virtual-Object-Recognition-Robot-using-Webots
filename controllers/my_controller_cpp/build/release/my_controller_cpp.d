1de210ac7b3e7fe3650f1ca936c43f03236d3136
Palash Ranjan Roy <73553721+Roy101@users.noreply.github.com>
2021-12-04 17:46:39 +0600

    Add files via upload

---
+++ controllers/my_controller_cpp/build/release/my_controller_cpp.d
@@ -0,0 +1,8 @@
+build/release/my_controller_cpp.o: my_controller_cpp.cpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/User/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Users/User/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h
